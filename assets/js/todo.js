948f797cd9c8d7188acf5f913fd78d3e5c6e8cb5
fabriciosiqueira08 <henriquefabricio008@gmail.com>
2022-12-14 21:22:41 -0300

    Percorrendo Array com ForEach

---
+++ assets/js/todo.js
@@ -0,0 +1,32 @@
+let data = [
+{
+    id: 1,
+    title: 'Estudar HTML'
+ }, 
+{
+         id: 2,
+        title: 'Estudar CSS'
+},
+{
+        id: 3,
+        title: 'Estudar JavaScript'
+},
+{
+        id: 4,
+        title: 'Estudar PHP'
+}
+];
+
+data.forEach(task => {
+    let li = document.createElement('li');
+
+    li.innerHTML = `
+
+        <input type="checkbox" id="task-${task.id}">
+        <label for="task-${task.id}">${task.title}</label>
+    
+    `;
+
+    document.querySelector('.todo').append(li);
+}
+    )
